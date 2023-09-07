67139418970ca623f93d369ac283d6525f6680e6
Jeffrey Chen <jeff@Jeffreys-MacBook-Pro.local>
2023-09-07 16:51:54 -0400

    fixing set up script bug

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Creating symbolic links from home directory to files here"
-for f in ".bashrc .bash_profile .portable_aliases .tmux.conf .vimrc" do
+for f in .bashrc .bash_profile .portable_aliases .tmux.conf .vimrc
+do
     ln -snf $f "~/$f"
 done
 
