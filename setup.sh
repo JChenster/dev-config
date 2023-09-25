c2cdfe08731ddf932f2bc26c4b3bca4d6f66e1d6
Jeffrey Chen <jeff@Jeffreys-MacBook-Pro.local>
2023-09-25 19:31:35 -0400

    fixed name of portable aliases in setup script and making tmux display msg for copying to clipboard less invasive

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating symbolic links from home directory to files here"
-for f in .bashrc .bash_profile .portable_aliases .tmux.conf .vimrc
+for f in .bashrc .bash_profile .portable_aliases.bash .tmux.conf .vimrc
 do
     ln -snf $(pwd)/$f ~/$f
     echo "mapped ~/$f -> $(pwd)/$f"
